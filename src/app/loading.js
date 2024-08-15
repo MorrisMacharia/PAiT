8a4b39eeb697aefceadb4feff88e29d0152545ea
MorrisMacharia <mmmchiuri@gmail.com>
2024-08-15 13:43:10 +0300

    load image

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,13 +3,11 @@ import Image from "next/image";
 
 export default function Loading() {
   return (
-    <h1 style={{ color: "#fff" }}>
       <Image
       src="/Group.png"
       alt="pait logo"
       width={257.893}
       height={71.63}
        />
-    </h1>
   );
 }
